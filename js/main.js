1f97b5f68fd237f7f1aa007720cb1a07a31a45de
YasuryOlivier <olivierbarriosyasury@gmail.com>
2016-12-06 15:31:27 -0600

    version final con sass

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$( document).ready(function(){
+    
+    $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav("hiden");
+    //$('.materialboxed').materialbo$(".button-collapse").sideNav();x();
+    $('.slider').slider({full_width: true});
+});
